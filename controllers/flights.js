e0305507a1544ae092d72f291e4395f9ceb58aa8
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-02-28 17:43:59 -0600

    handle form submission, redirect, list all flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,9 +1,34 @@
 import { Flight } from "../models/flight.js"
 
 function newFlight(req, res) {
-  res.render('flights/new')
+  res.render('flights/new', {
+    title: "Add Flight"
+  })
+}
+
+function create(req, res) {
+  for(let key in req.body) {
+    if(req.body[key] === '') delete req.body[key]
+  }
+  const flight = new Flight(req.body)
+  flight.save(function(error){
+    if (error) return res.render('flights/new')
+    res.redirect('/flights')
+  })
+}
+
+function index(req, res) {
+  Flight.find({}, function(error, flights){
+    res.render('flights/index', {
+      flights: flights,
+      error: error,
+      title: 'All Flights',
+    })
+  })
 }
 
 export {
   newFlight as new,
+  create,
+  index,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -3,7 +3,11 @@ const router = Router()
 import * as flightsCtrl from '../controllers/flights.js'
 
 /* GET users listing. */
+router.get('/', flightsCtrl.index)
 router.get('/new', flightsCtrl.new)
+
+//POST- 3000/
+router.post('/', flightsCtrl.create)
 
 export {
   router
