d97f1fa5da1c686e9b538841475d96b081082207
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-02-28 18:09:01 -0600

    link env to azure

--- models/flight.js
+++ models/flight.js
@@ -21,8 +21,8 @@ const flightSchema = new Schema({
   
   departs:{
     type: Date,
-    default: function() {
-      return new Date(new Date().setFullYear(new Date().getFullYear()+1))
+    default: function(){
+      return new Date().getFullYear()
     }
   } 
 }, {
